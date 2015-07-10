9528be12a0ee41b0ac6d1c95dc793a66793774e8
Brett Wilson <brettwil@microsoft.com>
2015-07-09 18:16:36 -0700

    Add NICs and load Balancer rules

--- AzureResourceGroup/Deploy.ps1
+++ AzureResourceGroup/Deploy.ps1
@@ -1,4 +1,5 @@
 ﻿Set-Location "C:\Users\brettwil\Documents\GitHub\USDX-AIO-Demo\AzureResourceGroup"
+Switch-AzureMode -Name AzureResourceManager
 
 $VerbosePreference = "Continue"                     # set to SilentlyContinue to suppress output
 $templateFile = '.\azuredeploy.json'
